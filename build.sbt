b48ee1702ff09007fc3d4bb16d18320e143c853e
ZhivkaGucevska <zhivka.gucevska@epfl.ch>
2015-03-27 15:20:27 +0100

    Modifies position setting

--- build.sbt
+++ build.sbt
@@ -1,10 +1,7 @@
 name := """macro-plugin"""
-
 version := "1.0"
-
 scalaVersion := "2.11.5"
-
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 libraryDependencies += "org.scala-lang" % "scala-compiler" % "2.11.1"
-resourceDirectory in Compile <<= baseDirectory(_ / "src" /"main" / "scala" /  "resources")
 
+resourceDirectory in Compile <<= baseDirectory(_ / "src" / "main" / "scala" / "resources")
--- src/main/scala/plugin/base/MacroDebugComponent.scala
+++ src/main/scala/plugin/base/MacroDebugComponent.scala
@@ -1,9 +1,10 @@
 package plugin.base
 
-import scala.reflect.internal.util.{OffsetPosition, BatchSourceFile}
-import scala.reflect.runtime.{universe => ru}
+import scala.reflect.internal.util.BatchSourceFile
 import scala.tools.nsc.plugins.PluginComponent
 import scala.tools.nsc.{Global, Phase}
+
+//import scala.sprinter.printers.PrettyPrinters
 
 
 /**
@@ -19,13 +20,22 @@ class MacroDebugComponent(val global: Global) extends PluginComponent {
   override val description = "generates synthetic code for macro expansions"
 
   val phaseName = "macro-debug"
+  /*
+   * Positions of the generated macro code will be created from the original position
+   * of the macro application. If the macro application is at line x, then the generated
+   * code for that macro will start at position x*assumedMacroLength
+   */
+  val assumedMacroLength = 100
 
-  case class MacroExpansion(expandee: Tree, originalLineInFile: Int, offInLine: Int)
+  /*
+   *
+   */
+  case class MacroExpansion(expandee: Tree, originalLineInFile: Int, posInLine: Int)
 
   override def newPhase(prev: Phase): StdPhase = new StdPhase(prev) {
     override def apply(unit: CompilationUnit): Unit = {
 
-      def appendExpansions(tree: Tree): BatchSourceFile = {
+      def appendExpansions(tree: Tree) = {
         // see Global.newSourceFile
         val source = unit.source.asInstanceOf[BatchSourceFile]
         //get number of lines in the original file, will change over time
@@ -34,62 +44,67 @@ class MacroDebugComponent(val global: Global) extends PluginComponent {
 
         var linesInFile = source.calculateLineIndices(source.content).length
         var expansionsDetected = List[MacroExpansion]()
+        var expandedDetected = List[MacroExpansion]()
 
         new Traverser {
           override def traverse(tree: Tree): Unit = {
             tree.attachments.get[analyzer.MacroExpansionAttachment] match {
               case Some(a@analyzer.MacroExpansionAttachment(expandee: Tree, expanded: Tree)) =>
                 val expansionString = showCode(expanded)
-                val posInFile = source.offsetToLine(tree.pos.start) + 1 //lines start at 0
-              //need to append empty lines until the line of beginning the macro
-              //Bad assumption for the line numbers, can easily find cases where this doesn't work. Also there is the implicit assumption that no macro is larger than 100 lines
-              val emptyLines = "\n" * (posInFile * 100 - linesInFile)
-                code.append(emptyLines)
-                if (emptyLines.length != 0) {
-                  code.append(expansionString)
-                  linesInFile = posInFile * 100 + expansionString.count(x => x == '\u000A' || x == '\u000D')
-                  expansionsDetected = MacroExpansion(expandee, posInFile, 0) :: expansionsDetected
-                }
+                val posInFile = source.offsetToLine(tree.pos.start)
+                val emptyLines = Array.fill((posInFile + 1) * assumedMacroLength - linesInFile + 1)('\n') //lines start at 0
+                code.appendAll(emptyLines)
+                code.append(expansionString)
+                linesInFile = (posInFile + 1) * assumedMacroLength + expansionString.count(x => x == '\u000A' || x == '\u000D')
+                expansionsDetected = MacroExpansion(expandee, posInFile, 0) :: expansionsDetected
+                expandedDetected = MacroExpansion(expanded, posInFile, 0) :: expandedDetected
               case _ => // do nothing
             }
             super.traverse(tree)
           }
-
         }.traverse(tree)
 
         val synSource = new BatchSourceFile(source.file.canonicalPath, code)
-        setPositionsExpansions(expansionsDetected, synSource)
+        setPositionsToExpansions(expansionsDetected, synSource)
+        setPositionsToExpanded(expandedDetected, synSource)
 
         //setPositionsTrees(tree, synSource)
-        synSource
 
       }
 
-      def setPositionsExpansions(expansions: List[MacroExpansion], synSource: BatchSourceFile): Unit = {
-        for (mExpansion <- expansions) {
-          setPositionsExpansions(mExpansion.expandee.children.map(a => MacroExpansion(a, mExpansion.originalLineInFile, a.pos.start - mExpansion.expandee.pos.start)), synSource)
-          mExpansion.expandee.setPos(Position.offset(synSource, synSource.lineToOffset((mExpansion.originalLineInFile - 1) * 100) + mExpansion.offInLine)) //lines start at 0
-
-        }
-
-      }
-
-      def setPositionsTrees(t: Tree, source: BatchSourceFile): Unit = {
-        if (t.pos != NoPosition) {
-          t.setPos(Position.offset(source, t.pos.point))
-        } else {
-
-        }
-        for (c <- t.children) setPositionsTrees(c, source)
-
-      }
 
       appendExpansions(unit.body)
-      /*val sourceTerm = ru.typeOf[CompilationUnit].decl(ru.TermName("source")).asMethod
-      val instanceMirror = scala.reflect.runtime.currentMirror.reflect(unit)
-      instanceMirror.reflectField(sourceTerm).set(sourceWithExpansions)*/
 
 
     }
   }
+
+  private def setPositionsToExpansions(expansions: List[MacroExpansion], synSource: BatchSourceFile): Unit = {
+    for (mExpansion <- expansions) {
+      setPositionsToExpansions(mExpansion.expandee.children.map {
+        a =>
+          if (a.pos != NoPosition && mExpansion.expandee.pos != NoPosition) {
+            MacroExpansion(a, mExpansion.originalLineInFile, a.pos.start - mExpansion.expandee.pos.start)
+          } else {
+            MacroExpansion(a, mExpansion.originalLineInFile, 0)
+          }
+      }, synSource)
+      mExpansion.expandee.setPos(Position.offset(synSource, synSource.lineToOffset((mExpansion.originalLineInFile + 1) * assumedMacroLength) + mExpansion.posInLine))
+    }
+  }
+
+  private def setPositionsTrees(t: Tree, source: BatchSourceFile): Unit = {
+    if (t.pos != NoPosition) {
+      t.setPos(Position.offset(source, t.pos.point))
+    } else {
+    }
+    for (c <- t.children) setPositionsTrees(c, source)
+  }
+
+  private def setPositionsToExpanded(expanded: List[MacroExpansion], synSource: BatchSourceFile): Unit = {
+    for (x <- expanded) {
+      x.expandee.setPos(Position.offset(synSource, synSource.lineToOffset(x.originalLineInFile * assumedMacroLength + 1)))
+    }
+  }
+
 }
